1a5970d765f520e172d6b38cf26d34f02d5146e3
Charlene <cjlava17@yahoo.com>
2023-10-18 16:27:45 +0200

    first commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Charlene Lava on 10/18/23.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
